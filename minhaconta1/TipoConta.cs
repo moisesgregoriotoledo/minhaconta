59d4dfccc23be7e700eea46d6656d0ac20949e0f
moisesgregoriotoledo <79237284+moisesgregoriotoledo@users.noreply.github.com>
2021-03-15 22:20:30 -0300

    Adicionar arquivos de projeto.

---
+++ minhaconta1/TipoConta.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace minhaconta1
+{
+    public enum TipoConta
+    {
+        PessoaFisica = 1,
+        PessoaJuridica = 2
+    }
+}
